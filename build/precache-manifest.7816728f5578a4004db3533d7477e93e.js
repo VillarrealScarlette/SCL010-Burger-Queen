cc506b2a3e114d9879dd6b7252f90c833083be07
anabelcarrion <anabelcarrion3110@gmail.com>
2019-09-11 14:15:07 -0300

    actualizando

---
+++ build/precache-manifest.7816728f5578a4004db3533d7477e93e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e1cb969587fe1d6417468a6651db017",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efa70e9288d28d14f30c",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f538ec34151b30b2aa9b",
+    "url": "/static/js/2.cbd2b41d.chunk.js"
+  },
+  {
+    "revision": "efa70e9288d28d14f30c",
+    "url": "/static/js/main.bd18ff2e.chunk.js"
+  },
+  {
+    "revision": "649f3179b6e0ac392e8a",
+    "url": "/static/js/runtime~main.86e6ade5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
