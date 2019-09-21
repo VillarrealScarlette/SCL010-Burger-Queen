098f0a424d4d15b9b524b37a7e76f90d85e4e206
anabelcarrion <anabelcarrion3110@gmail.com>
2019-09-21 13:58:08 -0300

    Realizando rutas hacia las distintas vistas

---
+++ src/components/OrdesInKitchen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+class OrdersInKitchen extends React.Component {
+  render() { 
+    return ( 
+      <div>
+        <h2>aqui van las ordenes que se le deben hacer a cocina</h2>
+      </div>
+     );
+  }
+}
+
+export default OrdersInKitchen;
