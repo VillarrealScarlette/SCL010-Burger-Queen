afabe8ea67ec5c3b5b663e693f38c4d0d604cd56
Scarlette Villarreal Á <villarrealscarlette@gmail.com>
2019-10-03 18:25:37 -0300

    Updates

---
+++ precache-manifest.483a8968a608565d3b02abadd97a44aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0be548a30f18fd390cba5572abfb11c",
+    "url": "/SCL010-Burger-Queen/index.html"
+  },
+  {
+    "revision": "0b4792668f516c061316",
+    "url": "/SCL010-Burger-Queen/static/css/main.e0da64da.chunk.css"
+  },
+  {
+    "revision": "202c93c05e43eaddab38",
+    "url": "/SCL010-Burger-Queen/static/js/2.93cad632.chunk.js"
+  },
+  {
+    "revision": "0b4792668f516c061316",
+    "url": "/SCL010-Burger-Queen/static/js/main.f51d09fd.chunk.js"
+  },
+  {
+    "revision": "43349e657b49b62a2aa8",
+    "url": "/SCL010-Burger-Queen/static/js/runtime~main.08914b4f.js"
+  }
+]);
